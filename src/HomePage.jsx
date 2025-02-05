974daeb4a04c6d0ba848faa803abe80f79102007
princeVillamil <jeffreyvillamilwork@gmail.com>
2025-02-06 00:35:10 +0800

    added a hero section - need to make globe img larger

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,11 +4,30 @@ import './assets/style/HomePage.css'
 import FooterComponent from './FooterComponent.jsx'
 import HeaderComponent from './HeaderComponent.jsx'
 
+import globeIMG from './assets/imgs/globeIMG.png'
+
 const HomePage = () =>{
   return (
     <div className="homePageContainer">
         <HeaderComponent/>
-        <h2 style={{ height: '30vh' }}></h2>
+        <section className="hero-section">
+          <div className="container">
+            <div className="image-container">
+              <img src={globeIMG} alt="" />
+            </div>
+            <div className="content">
+              <h3>
+                welcome to
+              </h3>
+              <h1>
+                DangerEye
+              </h1>
+              <p>Your no. 1 safety app for your daily travel.</p>
+              <a href="#" src="btn">Explore the map</a>
+            </div>
+
+          </div>
+        </section>
         <FooterComponent/>
     </div>
 
