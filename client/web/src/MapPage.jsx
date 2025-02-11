073ec198780eeeafe84c63b0af1b7f53ad99b7b7
Jelyka Mae Dizon <jelykamae05@gmail.com>
2025-02-11 21:03:04 +0800

    map things

--- client/web/src/MapPage.jsx
+++ client/web/src/MapPage.jsx
@@ -1,37 +1,18 @@
-import React from 'react';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
+import {React, Component} from 'react'
 import './assets/style/MapPage.css'
+import 'leaflet/dist/leaflet.css'
 
-const customIcon = new L.Icon({
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
+import { MapContainer, TileLayer } from 'react-leaflet'
 
-const MapPage = () => {
+function MapPage() {
   return (
-    <div className="map-wrapper">
-      <MapContainer 
-        center={[51.505, -0.09]} 
-        zoom={13} 
-        scrollWheelZoom={true} 
-        className="map-container"
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]} icon={customIcon}>
-          <Popup>
-            You are here! <br /> (London example)
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  </MapContainer>
   );
 };
 
-export default MapPage
+export default MapPage;
