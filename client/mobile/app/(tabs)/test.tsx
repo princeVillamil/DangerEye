0f85e515006eb8dc268ca1d30bcae59752e75d39
princeVillamil <jeffreyvillamilwork@gmail.com>
2025-03-04 08:31:38 +0800

    added react native template

---
+++ client/mobile/app/(tabs)/test.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet} from 'react-native'
+import React from 'react'
+
+const test = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  )
+}
+
+export default test
